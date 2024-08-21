63d5ecfa9bd5c23f355734e89f8dd8e9b3c55d45
Askis93 <erik.asklof@outlook.com>
2024-08-21 16:27:51 -0400

    Delete and list done

--- task-tracker.py
+++ task-tracker.py
@@ -24,12 +24,22 @@ def add_task():
 
     with open(task_file, "r") as file:
         data = json.load(file)
-        print(json.dumps(data))
+        print(json.dumps(data, indent=4))
 
 def delete_task():
-    print("Delete task")
 
-def list_tasks(status):
+    delete_task = args.delete
+
+    print("Delete task: " + delete_task)
+
+    with open(task_file, "r") as file:
+       data = json.load(file)
+       newdata = [obj for obj in data if obj.get('id') != int(delete_task)]
+       
+    with open(task_file, "w") as file:
+        json.dump(newdata, file, indent=4)
+
+def list_tasks():
     
     filter = args.list
     
@@ -37,7 +47,29 @@ def list_tasks(status):
         data = json.load(file)
         
         if filter == "all":
-            print(json.dumps(data))
+            print(json.dumps(data, indent=4))
+
+        if filter == "done":
+            tasks_done = []
+            for task in data:
+                if task.get("status") == "done":
+                    tasks_done.append(task)
+            print(json.dumps(tasks_done, indent=4))
+
+        if filter == "in-progess":
+            tasks_done = []
+            for task in data:
+                if task.get("status") == "in-progess":
+                    tasks_done.append(task)
+            print(json.dumps(tasks_done, indent=4))
+
+        if filter == "todo":
+            tasks_done = []
+            for task in data:
+                if task.get("status") == "todo":
+                    tasks_done.append(task)
+            print(json.dumps(tasks_done, indent=4))
+            
 
 def update_task():
     pass
@@ -54,4 +86,7 @@ if args.add:
     add_task()
 
 elif args.list:
-    list_tasks(str(args.list))
+    list_tasks()
+
+elif args.delete:
+    delete_task()
