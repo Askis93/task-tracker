1ecb78f5f6d6c255ece2cb3728278771691a4a76
Askis93 <erik.asklof@outlook.com>
2024-08-21 03:13:46 -0400

    Add functions

--- task-tracker.py
+++ task-tracker.py
@@ -6,7 +6,7 @@ from datetime import datetime
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-a", "--add", help="Add task to tracker", required=False)
-parser.add_argument("-l", "--list", help="Show all tasks, filter by [all, done, todo, in-progress]", required=False)
+parser.add_argument("-l", "--list", help="Show tasks, filter by [all, done, todo, in-progress]", const="all", nargs="?", required=False)
 parser.add_argument("-u", "--update", help="Update task", required=False)
 parser.add_argument("-d", "--delete", help="Delete task", required=False)
 parser.add_argument("-c", "--complete", help="Mark as done", required=False)
@@ -14,5 +14,44 @@ parser.add_argument("-p", "--progress", help="Mark as in progress", required=Fal
 
 args = parser.parse_args()
 
+task_file = "tasks.json"
+
+def add_task():
+
+    tasks = args.add
+
+    print("Add task: " + tasks)
+
+    with open(task_file, "r") as file:
+        data = json.load(file)
+        print(json.dumps(data))
+
+def delete_task():
+    print("Delete task")
+
+def list_tasks(status):
+    
+    filter = args.list
+    
+    with open(task_file, "r") as file:
+        data = json.load(file)
+        
+        if filter == "all":
+            print(json.dumps(data))
+
+def update_task():
+    pass
+
+def mark_as_done():
+    pass
+
+def mark_as_in_progress():
+    pass
+    
+
+
 if args.add:
-    print("hello")
+    add_task()
+
+elif args.list:
+    list_tasks(str(args.list))
